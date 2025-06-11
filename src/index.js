c25c0c0c284130b52d10cf15cbb922f9a1e5854e
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-06-11 17:23:15 +0530

    User Schema

--- src/index.js
+++ src/index.js
@@ -4,7 +4,17 @@ app.use(express.json());
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT;
+const mongoose = require("mongoose");
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
+
+mongoose
+  .connect("mongodb://localhost:27017/furturecodetechnology")
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
