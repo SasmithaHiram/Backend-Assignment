fecccbdeb8595ba4cd86aea46d381be761125c5c
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-06-12 01:01:30 +0530

    User Controller

---
+++ src/controller/userController.js
@@ -0,0 +1,22 @@
+const userService = require("../service/userService");
+
+exports.create = async (req, res) => {
+  try {
+    const user = userService.registerUser(req.body);
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+};
+
+exports.login = async (req, res) => {
+  try {
+    const user = userService.loginUser(req.body.username);
+    if (!user || user.password !== req.body.password) {
+      return res.status(401).json({ message: "Invalid credentials" });
+    }
+    res.json({ message: "Login successful" });
+  } catch (err) {
+    res.status(400).json({ error: err.message });
+  }
+};
