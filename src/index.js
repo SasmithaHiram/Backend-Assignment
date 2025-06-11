ef918e8fb301481d4669e81bed17bb07b919d6cc
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-06-12 01:25:33 +0530

    Update Index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
-//app.use(express.json());
 const dotenv = require("dotenv");
 dotenv.config();
 const port = process.env.PORT;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+const userRoutes = require("../src/routes/user.Routes");
+const productRoutes = require("../src/routes/product.Routes");
+
+app.use("/api/users", userRoutes);
+app.use("/api/prodcuts", productRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
