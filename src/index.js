a6a28e0cc6489b552e6b191477ab2915351cf1cc
Sasmitha Hiram <sasmithahiram2003@gmail.com>
2025-06-26 12:20:08 +0530

    cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const cors = require("cors");
 dotenv.config();
 const port = process.env.PORT || 3000;
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["POST", "PUT", "DELETE", "GET"],
+    credential: true,
+  })
+);
 app.use(bodyParser.json());
 const userRoutes = require("./routes/userRoutes");
 const productRoutes = require("./routes/productRoutes");
@@ -17,7 +25,9 @@ app.listen(port, () => {
 });
 
 mongoose
-  .connect("mongodb://localhost:27017/furturecodetechnology")
+  .connect(
+    "mongodb+srv://sasmithahiram2003:123@cluster0.7kvyryq.mongodb.net/furturecodetechnology?retryWrites=true&w=majority"
+  )
   .then(() => {
     console.log("Connected to MongoDB");
   })
